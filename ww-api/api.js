2e4164388ab0d52cd6b4d912c6d75f743bbde22f
Ryan M <mckeantech@gmail.com>
2023-05-08 18:53:10 -0700

    Started fixed API and Search file

--- ww-api/api.js
+++ ww-api/api.js
@@ -1,3 +1,16 @@
 const superagent = require('superagent');
 const base = 'https://wizard-world-api.herokuapp.com';
 
+const search = async (searchTerm) => {
+    try {
+        const searchURL = `${base}`;
+        const res = await superagent.get(searchURL);
+        return res.body;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+module.exports = {
+    search
+}
